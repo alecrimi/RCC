c20c79279872eac6498d7c4450a4149b4ba39f14
Alessandro Crimi <alessandro.crimi@iit.it>
2022-12-28 02:11:36 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,3 +42,11 @@ plt.legend()
 # Show the plot
 st.set_option('deprecation.showPyplotGlobalUse', False)
 st.pyplot()
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
