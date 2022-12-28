745a293d1c5f87f252dcb8a41576f6f44eaf68ea
Alessandro Crimi <alessandro.crimi@iit.it>
2022-12-28 01:25:19 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,12 +3,35 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Read in the data for the two time series
-series1 = pd.read_csv("series1.csv")
-series2 = pd.read_csv("series2.csv")
+#series1 = pd.read_csv("series1.csv")
+#series2 = pd.read_csv("series2.csv")
+x, y = [], []
+x.append(0.1)
+y.append(0.2)
+for i in range(1, 13):
+    xi = x[i-1]*(3.78-3.78*x[i-1])
+    yi = y[i-1]*(3.77-3.77*y[i-1])
+    x.append(xi)
+    y.append(yi)
+
+for i in range(13, 20001):
+	xi = x[i-1]*(3.78-3.78*x[i-1])
+	yi = y[i-1]*(3.77-3.77*y[i-1]-0.8*x[i-1-10])
+	x.append(xi)
+	y.append(yi)
+series1 = x
+series2 = y
+
+# Create a slider to control the parameter
+parameter = st.sidebar.slider("Parameter", 0, 100, 50)
+
+# Filter the data based on the parameter value
+filtered_series1 = series1[series1.parameter > parameter]
+filtered_series2 = series2[series2.parameter > parameter]
 
 # Create the plot
-plt.plot(series1.index, series1.value, label="Series 1")
-plt.plot(series2.index, series2.value, label="Series 2")
+plt.plot(filtered_series1.index, filtered_series1.value, label="Series 1")
+plt.plot(filtered_series2.index, filtered_series2.value, label="Series 2")
 
 # Add a legend
 plt.legend()
